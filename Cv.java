afe952704e2a91888a9b4d54a05c7c9874ae0677
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-07-26 16:24:06 +0530

    Create Cv.java

---
+++ Cv.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Cv
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		char ch = 'i';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println("Vowel");
+        else
+            System.out.println("Consonant");
+	}
+}
