6d52842574a138987e8e142e4c020af7808b5ddb
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-07-26 21:01:55 +0530

    Create Alpha.java

---
+++ Alpha.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Alpha
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		char ch=sc.next().charAt(0);
+		if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		{
+			System.out.println("Alphabet");
+		}
+		else
+		{
+			System.out.println("No");
+		}
+	}
+}
