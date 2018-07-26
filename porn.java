378f4a74be5d97a57339d4f5eaf36b3bf9ea4622
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-07-26 15:25:13 +0530

    Create porn.java

---
+++ porn.java
@@ -0,0 +1,20 @@
+class porn
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    if(n>0)
+    {
+      System.out.println("Positve");
+    }
+    else if(n<0)
+    {
+      System.out.println("Negative");
+    }
+    else
+    {
+      System.out.println("Zero");
+    }
+  }
+}
