cdcbcd0c34abfb22fae2bad6fac8954515dece7d
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-07-27 09:40:46 +0530

    Create Nat.java

---
+++ Nat.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Nat
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		for(int i=1;i<=n;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+}
