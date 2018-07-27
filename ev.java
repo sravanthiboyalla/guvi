961ca9a3755c940a1bece2e07ae22aabc0f7d96f
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-07-27 12:09:32 +0530

    Create ev.java

---
+++ ev.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ev
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int s=sc.nextInt();
+		int e=sc.nextInt();
+		for(int i=s;i<=e;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i );
+			}
+		}
+	}
+}
