a56dc0b04d5c601ecc8314a3fa4627e6a7b88918
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-07-27 11:37:19 +0530

    Create pal.java

---
+++ pal.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class java
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int n1;
+		int b=n;
+		int rev=0;
+		while(n>0)
+		{
+			n1=n%10;
+			rev=rev*10+n1;
+			n=n/10;
+		}
+		if(b==rev)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
