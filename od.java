afe13caad69ab649b36a6faeb8cd031a3a16dfa8
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-07-27 12:00:26 +0530

    Create od.java

---
+++ od.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class od
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int s=sc.nextInt();
+		int e=sc.nextInt();
+		for(int i=s;i<=e;i++)
+		{
+			if(i%2==1)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
