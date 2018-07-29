ffdffcea35411c1163f677bc626be841b765a6a1
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-07-29 22:26:06 +0530

    Create special1.java

---
+++ special1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class special1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		int count=0;
+		for(int i=0;i<str.length();i++)
+		{
+			if(!Character.isDigit(str.charAt(i))&&!Character.isAlphabetic(str.charAt(i)))
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+			
+	}
+}
