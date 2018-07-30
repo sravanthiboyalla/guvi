8eabb826d4f385a7a1d54d4a8dea8134ba8e7da3
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-07-30 07:50:24 +0530

    Create among.java

---
+++ among.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class among
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a[]=new int[10];
+		for(int i=0;i<10;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int max=a[0];
+		for(int i=0;i<10;i++)
+		{
+			if(max<a[i])
+			{
+				max=a[i];
+			}
+		}
+		System.out.println(max);
+	}
+}
