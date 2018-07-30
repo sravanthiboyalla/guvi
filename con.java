4e0364ffbbabe93fd9dc7ede9a73016b5d0b38ec
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-07-30 14:30:53 +0530

    Create con.java

---
+++ con.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class con
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		String s2=sc.nextLine();
+		String result=s1+s2;
+		System.out.println(result);
+	}
+}
