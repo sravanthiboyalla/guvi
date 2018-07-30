5a828991801e43f75568940285746cd67e9fdfe2
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-07-30 18:31:49 +0530

    Create adde.java

---
+++ adde.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class adde
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		if((a%2==0&&b%2==0)||(a%2==1&&b%2==1))
+		{
+			System.out.println("even");
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+	}
+}
