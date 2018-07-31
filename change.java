6ff31e6cee40952c8340b0e725ed0fb6c00712db
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-07-31 15:35:23 +0530

    Create change.java

---
+++ change.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class change
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		char ch=sc.next().charAt(0);
+		int i=ch;
+		System.out.println(i);
+	}
+}
