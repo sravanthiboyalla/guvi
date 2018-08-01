0c2ccf28aafef726d910fab04dd28739bad300d9
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-01 14:04:35 +0530

    Create va.java

---
+++ va.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class va
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int area=2*(a*b+b*c+c*a);
+		int vol=(a*b*c);
+		System.out.println(area+" "+vol);
+	}
+}
