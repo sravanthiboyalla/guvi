b737e8a815b0e14e903e50fc8ebaf6d96dfebd2b
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-01 22:00:06 +0530

    Update factt.java

--- factt.java
+++ factt.java
@@ -1,7 +1,6 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-of the class has to be "Main" only if the class is public. */
 class factt
 {
 	public static void main (String[] args) throws java.lang.Exception
