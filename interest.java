e90680e764010e8a8ec28ed261b90dd62d55dd4b
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-01 15:31:12 +0530

    Create interest.java

---
+++ interest.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class interest
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		float d=(a*b*c)/100;
+		int e=(int)(Math.floor(d));
+		System.out.println(e);
+	}
+}
