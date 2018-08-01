70379e018384aa53c2b421482c36bc6c7b97fcec
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-01 15:00:26 +0530

    Create temp.java

---
+++ temp.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class temp
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=a+273;
+		System.out.println(b);
+	}
+}
