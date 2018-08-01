6739e97e776671fc1f5d316bef7770ffc84a048d
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-01 22:31:12 +0530

    Create remspa.java

---
+++ remspa.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class remspa
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String result="";
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)!=' ')
+			{
+				result=result+s.charAt(i);
+			}
+		}
+		System.out.println(result);
+	}
+}
