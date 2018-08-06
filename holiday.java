27d820af18f3b083c918958c5718509478f001ac
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-06 14:38:54 +0530

    Create holiday.java

---
+++ holiday.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class holiday
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		if(s.equals("Sunday"))
+			System.out.println("yes");
+		else
+			System.out.println("no");
+	}
+}
