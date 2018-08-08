1e6a61c4827aaff7273bd345648dc98ce54f2ba6
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-08 22:32:42 +0530

    Create md.java

---
+++ md.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class md
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d=(a*b)/c;
+		System.out.println(d);
+	}
+}
