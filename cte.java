22da97defd641a45f0ed99f37581ac3d72e8dd87
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-09 20:10:16 +0530

    Create cte.java

---
+++ cte.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class cte
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		int k=sc.nextInt();
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==k)
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
