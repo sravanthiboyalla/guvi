f9676d6ada42eeaee1e0c551cffb068d93da2716
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-10 13:14:49 +0530

    Create meas.java

---
+++ meas.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class meas
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=n/10;
+		a=(a*10)+10;
+		System.out.println(a);
+	}
+}
