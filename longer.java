1f5a8bbe51e31849f8542aefeb485aafa7c8c6d1
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-10 12:32:21 +0530

    Update and rename long.java to longer.java

---
+++ longer.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class longer
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c;
+		if(a>b)
+		{
+			c=a-b;
+		}
+		else
+		{
+			c=b-a;
+		}
+		System.out.println(c);
+	}
+}
