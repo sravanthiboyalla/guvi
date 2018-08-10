88ed4769819215a15694fb0a1e490ed69c4e54fe
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-10 21:14:34 +0530

    Create linee.java

---
+++ linee.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class linee
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int x=sc.nextInt();
+		int d=(a*x)+b;
+		System.out.println(d);
+	}
+}
