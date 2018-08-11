d49c6eaebad142fc08c1375581143e3959620fb8
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-11 10:26:46 +0530

    Update and rename prnum.java to prinum.java

---
+++ prinum.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class prinum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int count1=0;
+		for(int i=a;i<=b;i++)
+		{
+			int count=0;
+			for(int j=1;j<=i;j++)
+			{
+				if(i%j==0)
+				{
+					count++;
+				}
+			}
+			if(count==2)
+			{
+				count1++;
+			}
+		}
+		System.out.println(count1);
+	}
+}
