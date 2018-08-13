1f8ba44b8de3fa4881f3242a24a154673e2b5f15
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-13 12:41:39 +0530

    Create cro.java

---
+++ cro.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class cro
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int rev=0;
+		int re=0;
+		while(n>0)
+		{
+			int d=n%10;
+			rev=rev*10+d;
+			n=n/10;
+		}
+		int l=String.valueOf(n).length();
+		while(rev>0)
+		{
+			int s=rev%10;
+			if(s%2==1)
+			{
+				System.out.print(s+" ");	
+			}
+			rev=rev/10;
+		}
+		
+	}
+}
