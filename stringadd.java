f4ae7667c615a75a706b999b45871b3d9ca1d4d0
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-14 23:52:49 +0530

    Create stringadd.java

---
+++ stringadd.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class stringadd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String d=s+" Answer";;
+		System.out.println(d);
+	}
+}
