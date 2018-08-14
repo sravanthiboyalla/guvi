defd923bb7e3daa76545f99cdb647aa02994a425
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-14 10:39:07 +0530

    Create diffb.java

---
+++ diffb.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class diffb
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.next();
+		int count=0;
+		for(int i=0;i<s1.length();i++)
+		{
+			if(s1.charAt(i)==s2.charAt(i))
+			{
+				count++;
+			}
+		}
+		if(s1.length()==(count+1))
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
