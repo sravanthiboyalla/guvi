c9d4ffc07efa1666aea7b716b1b336d8928c9e10
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-14 16:38:11 +0530

    Create maxdiff.java

---
+++ maxdiff.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class maxdiff
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		Arrays.sort(a);
+		int b=a[n-1]-a[0];
+		System.out.println(b);
+	}
+}
