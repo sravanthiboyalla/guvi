387d0894d1921621df641e1d53561ca0a9de2bd0
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-14 16:08:13 +0530

    Update viceversa.java

--- viceversa.java
+++ viceversa.java
@@ -9,7 +9,7 @@ class viceversa
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-		String s=sc.next();
+		String s=sc.nextLine();
 		String re="";
 		for(int i=0;i<s.length();i++)
 		{
