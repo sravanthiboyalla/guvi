fcd164c87394491377b1dad2fcca7cd0cead9b53
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-15 20:33:24 +0530

    Update and rename maxdiff.java to maxdiff1.java

--- maxdiff1.java
+++ maxdiff1.java
@@ -4,7 +4,7 @@ import java.lang.*;
 import java.io.*;
  
 /* Name of the class has to be "Main" only if the class is public. */
-class maxdiff
+class maxdiff1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
