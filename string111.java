e3f6e273da91fc47347f473a74e0dd8bef6f1862
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-20 22:11:34 +0530

    Create string111.java

---
+++ string111.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class string111
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int count=0;
+		int count1=0;
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)=='a'||s.charAt(i)=='b')
+			{
+				count++;
+			}
+			else
+			{
+				count1++;
+			}
+		}
+		if(count==s.length()-1)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
