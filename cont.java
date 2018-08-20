8a61288a248522765edb4fe0d318f1cbcdaafd04
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-20 18:09:21 +0530

    Create cont.java

---
+++ cont.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class cont
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int count=0;
+		for(int i=0;i<s.length();i++)
+		{
+			if(s.charAt(i)=='a'||s.charAt(i)=='b')
+			{
+				count++;
+			}
+		}
+		if(count==s.length())
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
