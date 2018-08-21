cb1a418402649759c130b1673a6fc673576aaff9
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-21 09:34:52 +0530

    Create sa.java

---
+++ sa.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class sa
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int sub=0;
+		for(int i=1;i<=n;i++)
+		{
+			sub=sub+i;
+		}
+		System.out.println(sub);
+	}
+}
