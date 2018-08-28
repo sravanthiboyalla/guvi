0909f48cffc7dcabf8570d87bc784241c0b1cf9a
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-28 10:25:35 +0530

    Create circular.java

---
+++ circular.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class circular
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		for(int i=0;i<s.length();i++)
+		{
+			if((s.charAt(i)==s.charAt(s.length()-1))&&s.contains("R")&&s.contains("L"))
+			{
+				System.out.println("yes");
+			}
+			else
+			{
+				System.out.println("no");
+			}
+			break;
+		}
+	}
+}
