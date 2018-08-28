741cc3ba576c56b3bcbef755f251f029ed356942
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-28 10:56:54 +0530

    Update stanagram.java

--- stanagram.java
+++ stanagram.java
@@ -11,13 +11,20 @@ class stanagram
 		int count=0;
 		if(s.length()==s1.length())
 		{
-			if(s1.contains("D")&&s1.contains("h")&&s1.contains("o")&&s1.contains("n")&&s1.contains("i"))
+			if(!(s.equals(s1)))
 			{
-				System.out.println("yes");
+				System.out.println("no");	
 			}
 			else
 			{
-				System.out.println("no");
+				if(s1.contains("D")&&s1.contains("h")&&s1.contains("o")&&s1.contains("n")&&s1.contains("i"))
+				{
+					System.out.println("yes");
+				}
+				else
+				{
+					System.out.println("no");
+				}
 			}
 		}
 		else
