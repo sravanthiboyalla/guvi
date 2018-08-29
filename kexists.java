8ec5d22c886bd6bb767c2191f0cf549854cafb92
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-29 14:35:02 +0530

    Create kexists.java

---
+++ kexists.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class kexists
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int a[]=new int[n];
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==k)
+			{
+				count++;
+			}
+		}
+		if(count>0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
