45959938b1150bf6d738e122184835e3947df6b2
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-29 09:32:33 +0530

    Create onlyodd.java

---
+++ onlyodd.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class onlyodd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int digit=0;
+		int sum=0;
+		int rev=0;
+		while(n>0)
+		{
+			digit=n%10;
+			rev=rev*10+digit;
+			n=n/10;
+		}
+		int di=0;
+		int i=1;
+		int k=0;
+		while(rev>0)
+		{
+			di=rev%10;
+			if(i%2==1)
+			{
+				k=k+di;	
+			}
+			i++;
+			rev=rev/10;
+		}
+		if(k%2==0)
+		{
+			System.out.println("E");
+		}
+		else
+		{
+			System.out.println("O");
+		}
+	}
+}
