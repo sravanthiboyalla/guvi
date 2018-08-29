b1be3953fea43fd78b1479d07b97c76502f72cc8
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-29 09:59:00 +0530

    Create countre.java

---
+++ countre.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class countre
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int k=sc.nextInt();
+		int count=0;
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==k)
+			{
+				count++;
+			}
+		}
+		if(count>0)
+		{
+			System.out.println("yes"+" "+count);
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
