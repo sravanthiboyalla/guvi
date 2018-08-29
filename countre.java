e00b2e99a5dd44a529f0a46de00419b320ed2f8a
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-08-29 10:02:18 +0530

    Update countre.java

--- countre.java
+++ countre.java
@@ -9,12 +9,12 @@ class countre
 	{
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
+		int k=sc.nextInt();
 		int a[]=new int[n];
 		for(int i=0;i<n;i++)
 		{
 			a[i]=sc.nextInt();
 		}
-		int k=sc.nextInt();
 		int count=0;
 		for(int i=0;i<n;i++)
 		{
