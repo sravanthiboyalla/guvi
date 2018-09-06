c219d8befc7fc2a1a25b4523d9fac1db295a2d05
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-09-06 16:24:16 +0530

    Create binaryor.java

---
+++ binaryor.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class binaryor
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int n=a|b;
+		int count=0;
+		String k=Integer.toBinaryString(n);
+		for(int i=0;i<k.length();i++)
+		{
+			if(k.charAt(i)=='1')
+			{
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
