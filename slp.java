f35318ea2a4ebf191d70770d0056a24619d7f561
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-09-06 16:12:15 +0530

    Create slp.java

---
+++ slp.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class slp
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		int d=s.length();
+		int count=0;
+		for(int i=2;i<d;i++)
+		{
+			if(d%i==0)
+			{
+				count++;
+			}
+		}
+		if(count==0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
