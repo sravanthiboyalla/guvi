b539b0ac10d276856a837241f9050890bdc48e1b
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-09-11 14:51:42 +0530

    Create equal.java

---
+++ equal.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class equal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a+b;
+		if(n%c==0)
+		{
+			System.out.println("Yes");	
+		}
+		else
+		{
+			System.out.println("No");
+		}
+	}
+}
