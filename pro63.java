85a5f5e72482182fed7600909c3a3bec5f5e9c12
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-09-14 10:26:25 +0530

    Update pro63.java

--- pro63.java
+++ pro63.java
@@ -8,24 +8,29 @@ class pro63
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		Scanner sc=new Scanner(System.in);
-		String s=sc.next();
-		int max=1;
+			Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int max=0;
+		int max1=0;
 		for(int i=0;i<s.length();i++)
 		{
-			int count=1;
-			for(int j=0;j<=i;j++)
+			int count=0;
+			for(int j=0;j<i;j++)
 			{
-				if(s.charAt(i)!=s.charAt(j))
+				if(s.charAt(i)==s.charAt(j))
+				{
+					max1++;
+				}
+				else
 				{
 					count++;
 				}
 			}
-			if(count>max)
+			if(max1==1||max1==0)
 			{
 				max=count;
 			}
 		}
-		System.out.println(max);
+		System.out.println(max+1);
 	}
 }
