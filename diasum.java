95f3c7ebc3e7867e8a290f8799432dc9cdbf6359
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-10-10 21:52:40 +0530

    Create diasum.java

---
+++ diasum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class diasum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[][]=new int[n][n];
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				a[i][j]=sc.nextInt();
+			}
+		}
+		int sum=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i;j<=i;j++)
+			{
+				sum=sum+a[i][j];
+			}
+		}
+		System.out.println(sum);
+	}
+}
