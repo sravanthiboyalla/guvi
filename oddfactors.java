936c4366f4541e3ef386eee710f69fd35d81b3b2
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-11-12 22:09:09 +0530

    Create oddfactors.java

---
+++ oddfactors.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class oddfactors
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<n;i++)
+		{
+			if(n%i==0)
+			{
+				if(i%2==1)
+				{
+					if(i==1)
+					{
+						System.out.print(i);
+					}
+					else
+					{
+						System.out.print(" "+i);
+					}
+				}
+			}
+		}
+	}
+}
