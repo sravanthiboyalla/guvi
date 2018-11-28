a5e5bcd0d59a8a35bb9fa6491c0a4932eca1c14e
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-11-28 15:39:56 +0530

    Create bitwiseand.java

---
+++ bitwiseand.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class bitwiseand
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		ArrayList<Integer> b=new ArrayList<Integer>();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				int x=a[i]&a[j];
+				b.add(x);
+			}
+		}
+		System.out.println(Collections.max(b));
+	}
+}
