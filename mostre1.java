1cd4b575f99bd53c923d4362d58ab4e0ac65d85b
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2018-11-28 10:25:44 +0530

    Update and rename mostre.java to mostre1.java

---
+++ mostre1.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+/* Name of the class has to be "Main" only if the class is public. */
+class mostre1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int count=0;
+		int k=0;
+		for(int i=0;i<n;i++)
+		{
+			int count1=0;
+			for(int j=0;j<n;j++)
+			{
+				if(a[i]==a[j])
+				{
+					count1++;
+				}
+			}
+			if(count<count1)
+			{
+				count=count1;
+				k=a[i];
+			}
+		}
+		System.out.println(k);
+	}
+}
