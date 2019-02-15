fc40d0bfd6b910a3e9cbabd0e3f2a571e199ef4e
sravanthiboyalla <41731325+sravanthiboyalla@users.noreply.github.com>
2019-02-15 12:45:02 +0530

    Update ais.java

--- ais.java
+++ ais.java
@@ -18,10 +18,10 @@ class ais
 		{
 			a[i]=sc.nextInt();
 		}
-		int max=a[0];
-		int sum=0;
+		int max=0;
 		for(int i=0;i<n;i++)
 		{
+			int sum=0;
 			for(int j=0;j<n;j++)
 			{
 				for(int k=0;k<n;k++)
@@ -29,12 +29,13 @@ class ais
 					if((i<=j)&&(j<=k))
 					{
 						sum=p*a[i]+q*a[j]+r*a[k];
+						//System.out.println(sum);
+					}
+					if(sum>max)
+					{
+					max=sum;
 					}
 				}
-			}
-			if(sum>max)
-			{
-				max=sum;
 			}
 		}
 		System.out.println(max);
